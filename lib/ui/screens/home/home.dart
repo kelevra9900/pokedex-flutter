a50fecd83518e21e2cb7d4ac0fee4cfb1d482152
Roger Torres <torresroger445@gmail.com>
2022-06-04 19:50:08 -0500

    feat: :art: HomeScreen added
    in construction with bloc state

---
+++ lib/ui/screens/home/home.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pokedex_demo/config/images.dart';
+import 'package:pokedex_demo/domain/entities/pokemon_detail.dart';
+
+import 'package:pokedex_demo/states/pokemon/pokemon_bloc.dart';
+import 'package:pokedex_demo/states/pokemon/pokemon_event.dart';
+import 'package:pokedex_demo/states/pokemon/pokemon_selector.dart';
+import 'package:pokedex_demo/states/pokemon/pokemon_state.dart';
+import 'package:pokedex_demo/ui/widgets/main_app_bar.dart';
+import 'package:pokedex_demo/ui/widgets/pokeball_background.dart';
+import 'package:pokedex_demo/ui/widgets/pokemon_card.dart';
+import 'package:pokedex_demo/ui/widgets/pokemon_refresh_control.dart';
+part 'sections/home_grid.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return PokeballBackground(
+      child: Stack(
+        children: [
+          _PokemonGrid(),
+        ],
+      ),
+    );
+  }
+}
